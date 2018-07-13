8870ac02e39deeed0cfa805ddfff1e5e952f92a1
wzmli <wzmichael.li@gmail.com>
2018-07-12 23:56:47 -0400

    adding fixed effect estimate

--- collect_gls.R
+++ collect_gls.R
@@ -5,14 +5,16 @@ library(ggplot2)
 
 obs <- 50
 
-results_dir_glslist <- list.files(path="test",pattern="RIonly")
+results_dir_glslist <- list.files(path="test",pattern="0")
 
 glsdf <- data.frame(phylo.int = numeric(obs)
+  , intercept = numeric(obs)
+  , slope = numeric(obs)
   , model = numeric(obs)
   , time = numeric(obs)
 )
 
-df_glslist <- list(glsdf,glsdf,glsdf) #list(glsdf,glsdf,glsdf,glsdf,glsdf)
+df_glslist <- list(glsdf,glsdf,glsdf,glsdf,glsdf,glsdf)
 
 for(i in 1:length(results_dir_glslist)){
   results_dir <- results_dir_glslist[[i]]
@@ -20,18 +22,26 @@ for(i in 1:length(results_dir_glslist)){
   for(j in 1:length(ff)){
     gls_list <- readRDS(paste("test/",results_dir,"/",ff[[j]],sep=""))
     df_glslist[[i]][j,1] <- attr(resid(gls_list[[1]]),"std")[[1]]
-    df_glslist[[i]][j,2] <- results_dir
-    df_glslist[[i]][j,3] <- gls_list[[2]][[1]]
+    df_glslist[[i]][j,2] <- coef(gls_list[[1]])[1]
+    df_glslist[[i]][j,3] <- coef(gls_list[[1]])[2]
+    df_glslist[[i]][j,4] <- results_dir
+    df_glslist[[i]][j,5] <- gls_list[[2]][[1]]
   }
 }
 
-gls_df <- bind_rows(df_glslist)
+gls_df <- (bind_rows(df_glslist)
+     %>% select(-time)
+#     %>% group_by(model)
+     %>% gather(key="parameter",value="estimate",-model)
+     %>% mutate(model = factor(model,levels=c("TIP50","TIP200","RI50","RI200","RI500","RI1000")))
+)
 
-gg <- (ggplot(gls_df,aes(y=phylo.int,x=model,group=model))
+gg <- (ggplot(gls_df,aes(y=estimate,x=model,group=model))
   + geom_violin()
-  + geom_hline(yintercept = 4)
+  + facet_wrap(~parameter,nrow=3)
+#  + geom_hline(yintercept = 4)
   + theme_bw()
-  + ylab("phylo sd")
+#  + ylab("sd")
   )
 
 print(gg)
