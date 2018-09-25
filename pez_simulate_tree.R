8d3d40e4d5bb035177250aebb231a1b1dcc69f79
wzmli <wzmichael.li@gmail.com>
2018-09-25 18:02:03 -0400

    edit code

--- pez_simulate_tree.R
+++ pez_simulate_tree.R
@@ -1,40 +1,42 @@
-nspp <- 20
-nsite <- 15
+# Simulate a phylogeny that has a lot of phylogenetic signal (power = 1.3)
+phy <- compute.brlen(rtree(n = nspp), method = "Grafen", power = 1.3)
 
-# Simulate a phylogenetic tree
-phy <- compute.brlen(rtree(n = nspp), method = "Grafen", power = 1)
+# Simulate species means
+sd.sp <- 1
+mean.sp <- rTraitCont(phy, model = "BM", sigma=sd.sp^2)
+# Replicate values of mean.sp over sites
+Y.sp <- rep(mean.sp, times=nsite)
 
-# Generate two environmental variables that are randomly distributed
-# among sites. Only values of env1 are known in the dataset.
-sd.env <- 1
-env1 <- rnorm(nsite, sd=sd.env)
-env2 <- rnorm(nsite, sd=sd.env)
-env2 <- rep(env2, each=nspp)
+# Simulate site means
+sd.site <- 1
+mean.site <- rnorm(nsite, sd=sd.site)
+# Replicate values of mean.site over sp
+Y.site <- rep(mean.site, each=nspp)
 
-# Generate two traits that differ phylogenetically among species. 
-# Both traits govern the response of species to env1, 
-# but only the values of trait1 are known in the dataset.
-sd.trait <- 1
-trait1 <- rTraitCont(phy, model = "BM", sigma = sd.trait)
-trait2 <- rTraitCont(phy, model = "BM", sigma = sd.trait)
-trait2 <- rep(trait2, times=nsite)
+# Compute the covariance matrix for phylogenetic attraction.
+sd.attract <- 1
+Vphy <- vcv(phy)
 
-# Simulate a dataset. The terms 'f11*d$trait1*env1' and 'f21*d$trait2*env1'
-# give the trait-by-environment interactions.
-b0 <- 0
-b1 <- 1
-b2 <- 1
-c1 <- 1
-c2 <- 1
-f11 <- .5
-f21 <- .5
+# Standardize Vphy to have determinant = 1.
+Vphy <- Vphy/(det(Vphy)^(1/nspp))
+
+# Construct the overall covariance matrix for phylogenetic attraction.
+V <- kronecker(diag(nrow=nsite, ncol=nsite), Vphy)
+Y.attract <- array(t(rmvnorm(n=1, sigma=sd.attract^2*V)))
+
+# Simulate residual errors
 sd.e <- 1
-
 Y.e <- rnorm(nspp*nsite, sd=sd.e)
 
-d <- data.frame(sp=rep(phy$tip.label, times=nsite),
-	site=rep(1:nsite, each=nspp), trait1=rep(trait1,
-	times=nsite), env1=rep(env1, each=nspp))
-d$Y <- b0 + b1*d$trait1 + b2*trait2 + c1*d$env1 
-	+ c2*env2 + f11*d$trait1*d$env1 + f21*trait2*d$env1 
-	+ Y.e
+# Construct the dataset
+d <- data.frame(sp=rep(phy$tip.label, times=nsite), site=rep(1:nsite, each=nspp))
+
+# Simulate abundance data
+d$Y <- Y.sp + Y.site + Y.attract + Y.e
+
+# Analyze the model
+communityPGLMM(Y ~ 1 
+	+ (1|sp__) 
+	+ (1|site) 
+	+ (1|sp__@site), 
+	data=d, tree=phy)
