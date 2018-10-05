a26d20827fc2d5be080465fa1c7a51c2d57c8410
michael li <wzmichael.li@gmail.com>
2018-10-05 14:34:29 -0400

    random slope basically works

--- phylo_re_plot.R
+++ phylo_re_plot.R
@@ -8,16 +8,16 @@ library(ggtree)
 library(cowplot)
 library(Hmisc)
 
-tree_seed <- 101
+tree_seed <- 121
 
 set.seed(tree_seed)
-nspp <- 50
+nspp <- 1200
 nsite <- 10
-nrep <- 20
+nrep <- 10
 
 phy <- rtree(n = nspp)
 
-x <- rnorm(nspp*nrep, sd=1)
+X <- rnorm(nspp*nrep*nsite, sd=1)
 
 gg_tree <- (ggtree(phy)
   + geom_tiplab()
@@ -30,15 +30,15 @@ gg_tree <- (ggtree(phy)
 Vphy <- vcv(phy)
 
 physd.y <- 10
-physd.x <- 0
+physd.x <- 10
 
-sd.y <- 10
-sd.x <- 0 
+sd.y <- 5
+sd.x <- 2 
 
 sd.site <- 2
 sd.resid <- 1
 
-phycormat <- matrix(c(1,0,0,1),nrow=2)
+phycormat <- matrix(c(1,0.7,0.7,1),nrow=2)
 physdvec <- c(physd.y, physd.x)
 phyvarmat <- physdvec %*% t(physdvec)
 phycovmat <- phyvarmat * phycormat
@@ -47,18 +47,19 @@ physigma <- kronecker(phycovmat, Vphy)
 
 betas <- c(0,0)
 
-seed <- 204
+seed <- 192409
 
 set.seed(seed)
 
 b_phy <- MASS::mvrnorm(n=1, mu=rep(betas,each=nspp), Sigma=physigma)
 
 Y.phy <- rep(head(b_phy,nspp), each = nrep*nsite)
-X.phy <- rep(tail(b_phy,nspp), each = nrep*nsite) 
+X.phy <- rep(tail(b_phy,nspp), each = nrep*nsite)*X 
 
 ## each trait will have nrep*nsite repeats
 
-cormat <- matrix(c(1,0,0,1),nrow=2)
+cormat <- matrix(c(1,0.4,0.4,1),nrow=2)
+
 
 print(cormat)
 sdvec <- c(sd.y, sd.x)
@@ -73,27 +74,27 @@ b <- MASS::mvrnorm(n=1, mu=rep(betas,each=nspp), Sigma=sigma_b)
 
 Y.int <- rep(head(b,nspp), each = nrep*nsite)
 
-X.int <- rep(tail(b,nspp), each = nrep*nsite)
+X.int <- rep(tail(b,nspp), each = nrep*nsite)*X
 
 b_site <- rnorm(n=nsite, mean=0, sd=sd.site)
 
 Y.site <- rep(rep(b_site,each=1),nspp*nrep)
 
 Y.e <- rnorm(nspp*nrep*nsite, sd=sd.resid)
-X.e <- rnorm(nspp*nrep*nsite, sd=1)
 
 Y.phyint <- Y.phy + Y.int
-# X.phyint <- X.phy + X.int
-# X.phyint.e <- X.phyint + X.e
+X.phyint <- X.phy + X.int
 Y.phyint.site <- Y.phyint + Y.site
 Y.phyint.site.e <- Y.phyint + Y.site + Y.e
-# Y <- Y.phyint.e #+ X.phyint.e
+Y <- Y.phyint.site.e + X.phyint
 
 dat <- data.frame(sp = rep(rownames(Vphy), each = nrep*nsite)
+  , X
   , Y.phy
   , Y.phyint
   , Y.phyint.site
   , Y.phyint.site.e
+  , Y
   , site = rep(rep(1:nsite,each=1),nspp*nrep)
   # , Y
   # , X.phy
@@ -107,9 +108,9 @@ source("new_phylo_setup.R")
 
 phyZ <- phylo.to.Z(phy, stand=FALSE)
 
-mod1 <- phylo_lmm(Y.phyint.site.e~1
-  + (1 | obs)
-  + (1 | sp)
+mod1 <- phylo_lmm(Y~1+X
+  + (1 + X| obs)
+  + (1 + X| sp)
   + (1 | site)
   , data=dat
   , phylonm = c("sp","sp:site")
